497fabbbaf1d261e51c8e9d00a71b124decd5553
nelg62 <glen.harding.nz@gmail.com>
2024-05-13 20:54:15 +1200

    first commit up to exercise 1

---
+++ Module7/src/Components/Clock.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+// Renders a digital time that updates every second
+function Clock() {
+  const [date, setDate] = useState(new Date());
+  //   useEffect(() => {
+  //     // first arg is usually an arrow function
+  //     setInterval(() => tick(), 1000);
+  //     console.log("Clock component mounted");
+  //   }, []); // second arg is an array of dependencies
+
+  useEffect(() => {
+    let clockInterval = setInterval(() => tick(), 1000);
+    console.log("Clock component mounted");
+
+    // function returned from useEffect is run on unmount
+    // and used to cleanup any side effects
+    return () => {
+      console.log("Clock component unmounted");
+      clearInterval(clockInterval);
+    };
+  }, []);
+
+  const tick = () => {
+    setDate(new Date());
+    console.log("tick"); // track the effect frequency
+  };
+  return (
+    <div className="Clock">
+      <h3>Digital Clock</h3>
+      {date.toLocaleTimeString()}
+    </div>
+  );
+}
+
+function ClockDisplay() {
+  const [showClock, setShowClock] = useState(false);
+
+  const toggleClock = () => {
+    setShowClock(!showClock);
+  };
+
+  return (
+    <div className="ClockDisplay componentBox">
+      {showClock && <Clock />}
+      <button onClick={toggleClock}>Toggle Clock</button>
+    </div>
+  );
+}
+
+export { Clock, ClockDisplay };
---
+++ Module7/src/Components/index.js
@@ -0,0 +1,5 @@
+import { Clock, ClockDisplay } from "./Clock";
+import ActivityFinder from "./ActivityFinder";
+import BitcoinRates from "./BitcoinRates";
+
+export { Clock, ClockDisplay, ActivityFinder, BitcoinRates };
